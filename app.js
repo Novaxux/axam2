3708dc85115976646d72b0ca6dd83afd4c6150c4
Novaxux <manuelova@outlook.es>
2023-11-29 20:19:25 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+let arrayeventos=[];
+let entrada=0;
+
+function crear (){
+
+    let entrada= document.getElementById("entrada").value
+    arrayeventos.push(entrada);
+    enseñar_dom("lista_desordenada",arrayeventos);    
+
+}
+
+console.log(arrayeventos)
+function enseñar_dom(elemento,array){
+    document.getElementById(elemento).innerHTML="";
+     for (let i=0;i<array.length;i++){
+
+        document.getElementById(elemento).innerHTML+=
+        `<li>${array[i]}</li>`;
+    } 
+}
+
+function borrar(){
+    arrayeventos.pop();
+    enseñar_dom("lista_desordenada",arrayeventos);   
+}
